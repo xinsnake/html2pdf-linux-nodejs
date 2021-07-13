93a192350afbad16aae876f7f737d98406a5175c
Jacob Zhou <jazhou@microsoft.com>
2021-07-13 01:49:08 -0700

    Tweaks

--- FetchAndConvert/index.js
+++ FetchAndConvert/index.js
@@ -16,13 +16,20 @@ module.exports = async function (context, req) {
         return;
     }
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+        deviceScaleFactor: 1,
+    });
     await page.goto(url, {
-        waitUntil: 'networkidle2',
+        waitUntil: 'networkidle0',
+        timeout: 30000
     });
     await page.emulateMediaType('screen');
-    await page.pdf({ path: path, format: 'a4' });
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+    await page.pdf({ path: path, format: 'a4', landscape: true, printBackground: true });
     await browser.close();
 
     const fileBuffer = fs.readFileSync(path);
