1cd85ec14d17df9d6467936a226b72cc93a0f7c0
Jacob Zhou <jazhou@microsoft.com>
2021-07-12 21:03:44 -0700

    Try PDF

--- FetchAndConvert/index.js
+++ FetchAndConvert/index.js
@@ -1,13 +1,35 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+const path = `/tmp/html2pdf-${new Date().getTime()}-${Math.random().toString(36).substring(7)}.pdf`;
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const url = req.query.url;
+    if (!url) {
+        context.res = {
+            status: 400,
+            body: { error: 'query string "url" is required' }
+        };
+    }
 
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, {
+        waitUntil: 'networkidle2',
+    });
+    await page.emulateMediaType('screen');
+    await page.pdf({ path: path, format: 'a4' });
+    await browser.close();
+
+    const fileBuffer = fs.readFileSync(path);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        status: 200,
+        body: fileBuffer,
+        headers: {
+            "Content-Type": "application/pdf",
+            "Content-Disposition": "inline"
+        }
     };
 }
