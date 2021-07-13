a8f535ac5bbb4cba356cda124add864b94b8233b
Jacob Zhou <jazhou@microsoft.com>
2021-07-12 21:12:52 -0700

    Minor tweaks and fixes

--- FetchAndConvert/index.js
+++ FetchAndConvert/index.js
@@ -7,21 +7,32 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const url = req.query.url;
+    console.log(url);
     if (!url) {
         context.res = {
             status: 400,
             body: { error: 'query string "url" is required' }
         };
+        return;
     }
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url, {
-        waitUntil: 'networkidle2',
-    });
-    await page.emulateMediaType('screen');
-    await page.pdf({ path: path, format: 'a4' });
-    await browser.close();
+    try {
+        await page.goto(url, {
+            waitUntil: 'networkidle2',
+        });
+        await page.emulateMediaType('screen');
+        await page.pdf({ path: path, format: 'a4' });
+        await browser.close();
+    } catch (error) {
+        context.res = {
+            status: 500,
+            body: { error: error.message }
+        };
+        return;
+    }
+
 
     const fileBuffer = fs.readFileSync(path);
     context.res = {
